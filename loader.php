4f2d81ac6a0a1f8debd32365c9cde049b135540b
Boone Gorges <boonebgorges@gmail.com>
2011-11-30 14:58:29 -0500

    Some dummy structure to get started with testing

---
+++ loader.php
@@ -0,0 +1,74 @@
+<?php
+/*
+Plugin Name: Commons In A Box
+Plugin URI: http://github.com/cuny-academic-commons/
+Description: A suite of community and collaboration tools for WordPress, designed especially for academic communities
+Version: 0.1
+Author: CUNY Academic Commons
+Author URI: http://commons.gc.cuny.edu
+Licence: GPLv3
+Network: true
+*/
+
+class Commons_In_A_Box {
+	var $plugin_dir;
+	
+	/**
+	 * Static bootstrapping init method
+	 *
+	 * @since 0.1
+	 */
+	public static function &init() {
+		global $instance;
+		
+		if ( empty( $instance ) ) {
+			$instance = new Commons_In_A_Box; 
+		}
+		
+		return $instance;
+	}
+	
+	/**
+	 * Constructor
+	 *
+	 * @since 0.1
+	 */
+	public function __construct() {
+		$this->setup_globals();
+		$this->setup_hooks();
+		$this->includes();
+	}
+	
+	/**
+	 * Sets up some class data
+	 *
+	 * @since 0.1
+	 * @todo Rename. These aren't globals
+	 * @todo Figure out a reliable way to use plugin_dir_path()
+	 */
+	private function setup_globals() {
+		$this->plugin_dir = trailingslashit( dirname( __FILE__ ) );
+	}
+	
+	/**
+	 * Sets up WP hooks
+	 *
+	 * @since 0.1
+	 */
+	private function setup_hooks() {
+		
+	}
+	
+	/**
+	 * Includes necessary files
+	 *
+	 * @since 0.1
+	 * @todo Make this nice somehow
+	 */
+	private function includes() {
+		require( $this->plugin_dir . 'plugins/plugins-loader.php' );
+	}
+}
+add_action( 'init', array( 'Commons_In_A_Box', 'init' ) ); 
+
+?>
---
+++ plugins/plugins-loader.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Set up plugin management
+ *
+ * @package Commons_In_A_Box
+ * @subpackage Plugins
+ * @since 0.1
+ */
+
+?>
