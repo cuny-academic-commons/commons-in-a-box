0776e020c8136e6d16dc6cce1f5770377ddd079d
r-a-y <r-a-y@gmx.com>
2012-09-12 22:07:47 -0700

    Admin function tweaks:
    * Add cbox_is_upgraded() function - to see if CBox just upgraded
    
    * Add cbox_bump_revision_date() function - to bump the CBox revision date in the DB
    
    * Add cbox_the_bp_admin_wizard_url() - to output the URL for the BP admin wizard page

--- admin/functions.php
+++ admin/functions.php
@@ -17,16 +17,9 @@ if ( !defined( 'ABSPATH' ) ) exit;
  * @since 0.3
  */
 function cbox_is_setup() {
-	// get the installed revision date
-	$installed_revision_date = cbox_get_installed_revision_date();
-
-	// if empty, this means CBox never finished setting up
-	if ( empty( $installed_revision_date ) )
-		return false;
-
-	// if we're at this point, cbox is installed, but we just upgraded to a 
+	// cbox is installed, but we just upgraded to a 
 	// newer version of CBox
-	if ( cbox_get_current_revision_date() > $installed_revision_date )
+	if ( cbox_is_upgraded() )
 		return false;
 
 	// if BuddyPress doesn't exist, stop now
@@ -39,6 +32,21 @@ function cbox_is_setup() {
 		return false;
 
 	return true;
+}
+
+/**
+ * Check to see if CBox has just upgraded.
+ *
+ * @uses cbox_get_installed_revision_date() Gets the CBox revision date from the DB
+ * @uses cbox_get_current_revision_date() Gets the current CBox revision date from Commons_In_A_Box::setup_globals()
+ * @return bool
+ * @since 0.3
+ */
+function cbox_is_upgraded() {
+	if ( cbox_get_installed_revision_date() && ( cbox_get_current_revision_date() > cbox_get_installed_revision_date() ) )
+		return true;
+
+	return false;
 }
 
 /**
@@ -80,3 +88,39 @@ function cbox_get_current_revision_date() {
 function cbox_get_installed_revision_date() {
 	return strtotime( get_site_option( '_cbox_revision_date' ) );
 }
+
+/**
+ * Bumps the CBox revision date in the DB
+ *
+ * @since 0.3
+ */
+function cbox_bump_revision_date() {
+	update_site_option( '_cbox_revision_date', cbox()->revision_date );
+}
+
+/**
+ * Outputs the URL for the BP Admin Wizard page.
+ *
+ * @uses cbox_get_the_bp_admin_wizard_url() To get the URL for the BP Admin Wizard pgae.
+ * @since 0.3
+ */
+function cbox_the_bp_admin_wizard_url() {
+	echo cbox_get_the_bp_admin_wizard_url();
+}
+
+	/**
+	 * Get the URL for the BP Admin Wizard page.
+	 *
+	 * This basically copies a section of code from
+	 * {@link BP_Admin::admin_menus()}.
+	 *
+	 * @since 0.3
+	 */
+	function cbox_get_the_bp_admin_wizard_url() {
+		if ( ! is_multisite() || bp_is_multiblog_mode() ) {
+			return admin_url( 'index.php?page=bp-wizard' );
+		} else {
+			return network_admin_url( 'update-core.php?page=bp-wizard' );
+		}
+	}
+
