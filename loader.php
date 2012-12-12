9e1c94e19090a6f5cf3dcff0ebde3c77e274b5fa
r-a-y <r-a-y@gmx.com>
2012-12-12 12:16:07 -0800

    Add activation / deactivation hooks in CBOX.

--- loader.php
+++ loader.php
@@ -117,6 +117,10 @@ class Commons_In_A_Box {
 	 * @since 1.0-beta4
 	 */
 	private function setup_actions() {
+		// Add actions to plugin activation and deactivation hooks
+		add_action( 'activate_'   . plugin_basename( __FILE__ ), create_function( '', "do_action( 'cbox_activation' );"   ) );
+		add_action( 'deactivate_' . plugin_basename( __FILE__ ), create_function( '', "do_action( 'cbox_deactivation' );" ) );
+
 		// localization
 		// we only fire this in the admin area, since we have no strings to localize
 		// on the frontend... yet!
