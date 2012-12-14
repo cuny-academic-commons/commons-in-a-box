b9edd06fe35c394f02b31cf7acd163565ebc642d
r-a-y <r-a-y@gmx.com>
2012-12-14 08:52:57 -0800

    Update 'What's New' page for 1.0-beta4.

--- admin/changelog.php
+++ admin/changelog.php
@@ -16,42 +16,14 @@
 			</h2>
 
 			<div class="changelog">
-				<h3><?php _e( 'New Plugins', 'cbox' ); ?></h3>
-
-				<div class="feature-section images-stagger-right">
-					<img class="image-50" alt="" src="http://news.commons.gc.cuny.edu/files/2012/08/rbe8.jpg" />
-
-					<h4><?php _e( 'Reply By Email', 'cbox' ); ?></h4>
-					<p><?php printf( __( 'Participate in discussions on your site without leaving your inbox. When you receive a notification of a forum post, a private message, or a public @-mention, reply to the notification email, and your reply will be posted to your CBOX-powered site. <a href="%s">Find it under the "&Agrave; la carte" section</a>.  Also, be sure to <a href="%s">read the setup guide</a>.', 'cbox' ), self_admin_url( 'admin.php?page=cbox-plugins' ), 'https://github.com/r-a-y/bp-reply-by-email/wiki' ); ?></p>
-
-					<h4><?php _e( 'Group Announcements', 'cbox' ); ?></h4>
-					<p><?php printf( __( 'This plugin removes the "What\'s New" post form from group home pages and adds a new "Announcements" tab to groups. On the "Announcements" tab, all users can see updates to the group, and admins/mods can post new updates. The plugin was created to avoid confusion, which can occur when both discussion forums and activity posting are enabled in groups. <a href="%s">Find it under the "Recommended" section</a>.', 'cbox' ), self_admin_url( 'admin.php?page=cbox-plugins' ) ); ?></p>
-
-				</div>
-			</div>
-
-			<div class="changelog">
-				<h3><?php _e( 'New Feature', 'cbox' ); ?></h3>
-
-				<div class="feature-section">
-					<h4><?php _e( 'CBOX Settings Page', 'cbox' ); ?></h4>
-					<p><?php printf( __( 'You can now configure some special options for certain CBOX plugins all on one page. <a href="%s">Check it out here</a>.', 'cbox' ), self_admin_url( 'admin.php?page=cbox-settings' ) ); ?></p>
-
-				</div>
-			</div>
-
-			<div class="changelog">
 				<h3><?php _e( 'Plugin Updates', 'cbox' ); ?></h3>
 
 				<div class="feature-section">
 					<h4><?php _e( 'The following plugins were updated in CBOX:', 'cbox' ); ?></h4>
 					<ul>
-						<li>bbPress 2.2.2</li>
-						<li>BuddyPress Docs 1.2.6</li>
-						<li>BuddyPress Docs Wiki 1.0.2</li>
-						<li>BuddyPress Group Email Subscription 3.2.3</li>
-						<li>CAC Featured Content 1.0.3</li>
-						<li>External Group Blogs 1.5</li>
+						<li>BuddyPress 1.6.2</li>
+						<li>BuddyPress Group Email Subscription 3.3</li>
+						<li>bbPress 2.2.3</li>
 					</ul>
 
 				</div>
@@ -63,18 +35,40 @@
 				<div class="feature-section">
 					<h4><?php _e( 'The following bugfixes and enhancements were added to the Commons In A Box theme:', 'cbox' ); ?></h4>
 					<ul>
-						<li><?php _e( 'Fixed problem with theme options that caused them not to work on setups that forbid certain kinds of browser cookies', 'cbox' ) ?></li>
-						<li><?php _e( 'Better slider performance', 'cbox' ) ?></li>
-						<li><?php _e( 'Improved responsiveness', 'cbox' ) ?></li>
-						<li><?php _e( 'Better theme admin performance on Windows IIS servers', 'cbox' ) ?></li>
-						<li><?php _e( 'Improved current-page highlighting on BuddyPress item navigation', 'cbox' ) ?></li>
-						<li><?php _e( 'Fixed some broken image links', 'cbox' ) ?></li>
-						<li><?php _e( 'Improved styling for bbPress 2.2.x forum threads', 'cbox' ) ?></li>
-						<li><?php _e( 'Improved default menu configuration', 'cbox' ) ?></li>
-						<li><?php _e( 'Added a login/logout link to mobile navigation', 'cbox' ) ?></li>
+						<li>TODO</li>
 					</ul>
 
 				</div>
+			</div>
+
+			<div class="changelog">
+				<h3><?php _e( 'Under the Hood', 'cbox' ); ?></h3>
+
+				<div class="feature-section col two-col">
+					<div>
+						<h4><?php _e( 'Behind The Scenes Forum Integration', 'cbox' ); ?></h4>
+						<p><?php _e( "If you're installing CBOX and BuddyPress for the first time, CBOX will automatically configure the best forums setup for you.", 'cbox' ); ?></p>
+
+						<p><?php printf( __( "If you have an existing BuddyPress site and are using BP's Discussion Forums component, <a href='%s'>please read this article</a>.", 'cbox' ), 'https://github.com/cuny-academic-commons/commons-in-a-box' ); ?></p>
+					</div>
+
+
+
+					<div class="last-feature">
+						<h4><?php _e( 'WordPress Version Requirements', 'cbox' ); ?></h4>
+						<p><?php _e( "If you haven't updated WordPress, CBOX will show an upgrade notice in the CBOX dashboard asking you to upgrade to the latest compatible version.", 'cbox' ); ?></p>
+					</div>
+				</div>
+
+				<div class="feature-section col two-col">
+					<div>
+						<h4><?php _e( 'Localization Support', 'cbox' ); ?></h4>
+						<p><?php _e( 'CBOX is now fully localized.', 'cbox' ); ?></p>
+
+						<p><?php printf( __( "If you want to translate CBOX to another language, please <a href='%s'>contact us</a> or <a href='%s'>send us a pull request on Github</a> with your translation files and we will bundle it in the next version of the plugin.", 'cbox' ), 'mailto:commons@gc.cuny.edu', 'https://github.com/cuny-academic-commons/commons-in-a-box' ); ?></p>
+					</div>
+				</div>
+
 			</div>
 
 			<div class="return-to-dashboard">
