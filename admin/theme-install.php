3f96ce5ffca554cd131cfceea7736ce01dbe12fc
Boone B Gorges <boonebgorges@gmail.com>
2012-12-15 03:15:45 -0500

    Pin cbox-theme 1.0-beta4

--- admin/theme-install.php
+++ admin/theme-install.php
@@ -41,11 +41,9 @@ class CBox_Theme_Specs {
 	 */
 	private static $cbox_theme = array(
 		'name'           => 'Commons In A Box Theme',
-		'version'        => '1.0-beta2',
+		'version'        => '1.0-beta4',
 		'directory_name' => 'cbox-theme',
-
-		// Note the url 1.0beta2 - this is a workaround for a limitation in the way the theme folder is named
-		'download_url'   => 'http://github.com/cuny-academic-commons/cbox-theme/archive/1.0-beta2.zip'
+		'download_url'   => 'http://github.com/cuny-academic-commons/cbox-theme/archive/1.0-beta4.zip'
 	);
 
 	/**
