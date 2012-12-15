e20c8e7e0d3a35a1ddc247abc35b1638f207a077
Boone B Gorges <boonebgorges@gmail.com>
2012-12-15 03:48:54 -0500

    Bumps version number to 1.0-beta4

--- loader.php
+++ loader.php
@@ -3,7 +3,7 @@
 Plugin Name: Commons In A Box
 Plugin URI: http://commonsinabox.org
 Description: A suite of community and collaboration tools for WordPress, designed especially for academic communities
-Version: 1.0-beta3
+Version: 1.0-beta4
 Author: CUNY Academic Commons
 Author URI: http://commons.gc.cuny.edu
 Licence: GPLv3
