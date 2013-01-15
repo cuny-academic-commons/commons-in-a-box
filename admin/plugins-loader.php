f32df4dc2b510d66184d1e5615ae1e8803a0cf7a
r-a-y <r-a-y@gmx.com>
2013-01-14 16:10:15 -0800

    Pin BuddyPress 1.6.3.

--- admin/plugins-loader.php
+++ admin/plugins-loader.php
@@ -117,7 +117,7 @@ class CBox_Plugins {
 			'plugin_name'       => 'BuddyPress',
 			'cbox_name'         => __( 'BuddyPress', 'cbox' ),
 			'cbox_description'  => __( 'BuddyPress provides the core functionality of Commons In A Box, including groups and user profiles.', 'cbox' ),
-			'version'           => '1.6.2',
+			'version'           => '1.6.3',
 			'documentation_url' => 'http://commonsinabox.org/documentation/plugins/buddypress-plugin',
 			'admin_settings'    => 'options-general.php?page=bp-components',
 			'network_settings'  => 'settings.php?page=bp-components'
@@ -365,7 +365,7 @@ class CBox_Plugins {
 		self::register_plugin( array(
 			'plugin_name'  => 'BuddyPress',
 			'type'         => 'dependency',
-			'download_url' => 'http://downloads.wordpress.org/plugin/buddypress.1.6.2.zip'
+			'download_url' => 'http://downloads.wordpress.org/plugin/buddypress.1.6.3.zip'
 		) );
 	}
 
