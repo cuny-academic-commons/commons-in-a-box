7fa07ee8195d727876b4ccd5fad883dd62a70e46
Boone B Gorges <boonebgorges@gmail.com>
2013-02-14 09:31:10 -0500

    Pin BuddyPress Docs 1.2.10

--- admin/plugins-loader.php
+++ admin/plugins-loader.php
@@ -139,9 +139,9 @@ class CBox_Plugins {
 			'type'              => 'recommended',
 			'cbox_name'         => __( 'Docs', 'cbox' ),
 			'cbox_description'  => __( 'Allows your members to collaborate on wiki-style Docs.', 'cbox' ),
-			'version'           => '1.2.9',
+			'version'           => '1.2.10',
 			'depends'           => 'BuddyPress (>=1.5)',
-			'download_url'      => 'http://downloads.wordpress.org/plugin/buddypress-docs.1.2.9.zip',
+			'download_url'      => 'http://downloads.wordpress.org/plugin/buddypress-docs.1.2.10.zip',
 			'documentation_url' => 'http://commonsinabox.org/documentation/plugins/buddypress-docs',
 			'admin_settings'    => 'edit.php?post_type=bp_doc',
 			'network_settings'  => 'root-blog-only'
