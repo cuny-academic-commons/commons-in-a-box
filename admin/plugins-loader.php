7535e4471da468f123b4a2604ecf68ddbca71f0c
Boone B Gorges <boonebgorges@gmail.com>
2013-02-18 11:05:18 -0500

    Tag BP Group Announcements 1.0.2

--- admin/plugins-loader.php
+++ admin/plugins-loader.php
@@ -232,8 +232,8 @@ class CBox_Plugins {
 			'cbox_name'         => __( 'Group Announcements', 'cbox' ),
 			'cbox_description'  => __( 'Repurposes group activity updates, using an Announcements tab to groups.', 'cbox' ),
 			'depends'           => 'BuddyPress (>=1.5)',
-			'version'           => '1.0.1',
-			'download_url'      => 'http://github.com/cuny-academic-commons/bp-group-announcements/archive/1.0.1.zip',
+			'version'           => '1.0.2',
+			'download_url'      => 'http://github.com/cuny-academic-commons/bp-group-announcements/archive/1.0.2.zip',
 			'documentation_url' => 'http://commonsinabox.org/documentation/plugins/bp-group-announcements',
 		) );
 
