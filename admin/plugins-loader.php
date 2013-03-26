1985ff8b301e30ad375b42514ba0092559608cf8
r-a-y <r-a-y@gmx.com>
2013-03-25 22:27:26 -0700

    Pin bbPress 2.2.4.

--- admin/plugins-loader.php
+++ admin/plugins-loader.php
@@ -207,8 +207,8 @@ class CBox_Plugins {
 			'type'              => 'recommended',
 			'cbox_name'         => __( 'bbPress Forums', 'cbox' ),
 			'cbox_description'  => __( 'Sitewide and group-specific discussion forums.', 'cbox' ),
-			'version'           => '2.2.3',
-			'download_url'      => 'http://downloads.wordpress.org/plugin/bbpress.2.2.3.zip',
+			'version'           => '2.2.4',
+			'download_url'      => 'http://downloads.wordpress.org/plugin/bbpress.2.2.4.zip',
 			'documentation_url' => 'http://commonsinabox.org/documentation/plugins/bbpress',
 			'admin_settings'    => 'options-general.php?page=bbpress',
 			'network_settings'  => 'root-blog-only'
