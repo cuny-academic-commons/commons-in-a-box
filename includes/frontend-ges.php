fe7327279eb0d6661330b215eb65836ccfaec626
r-a-y <r-a-y@gmx.com>
2013-05-09 00:53:39 -0700

    Relax CBOX's default GES setting so it isn't forced.
    Previously, CBOX's default GES setting of 'All Mail' was overriding the
    group's own defined GES setting if a group admin wanted to change it.
    
    This commit only forces the 'All Mail' setting if the group's setting does
    not exist.

--- includes/frontend-ges.php
+++ includes/frontend-ges.php
@@ -31,19 +31,25 @@ class CBox_GES_All_Mail {
 
 	public function __construct() {
 		// changes the default group subscription level
-		add_filter( 'ass_default_subscription_level',       array( $this, 'default_group_email_setting' ) );
+		add_filter( 'ass_default_subscription_level', array( $this, 'default_group_email_setting' ), 20 );
 
 		// changes the setting under a group's "Admin > Members"
 		// specifically the "Site Admin Only" block
-		add_filter( 'ass_get_default_subscription',         array( $this, 'default_group_email_setting' ) );
+		add_filter( 'ass_get_default_subscription',   array( $this, 'default_group_email_setting' ) );
 	}
 
 	/**
-	 * Changes the group subscription level to "All Mail".
-	 * "supersub" = "All Mail"
+	 * Changes the group subscription level to "All Mail" if group does not have
+	 * a default subscription level set.
+	 *
+	 * ("supersub" = "All Mail")
 	 */
 	public function default_group_email_setting( $setting ) {
-		return 'supersub';
+		if ( empty( $setting ) ) {
+			return 'supersub';
+		} else {
+			return $setting;
+		}
 	}
 }
 
