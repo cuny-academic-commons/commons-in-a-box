8ae4795f9d5009a0cae35e601197606f44cadf3a
r-a-y <r-a-y@gmx.com>
2013-07-29 11:40:16 -0700

    Pin External Group Blogs 1.5.2.

--- admin/plugins-loader.php
+++ admin/plugins-loader.php
@@ -305,8 +305,8 @@ class CBox_Plugins {
 			'cbox_name'         => __( 'External RSS Feeds for Groups', 'cbox' ),
 			'cbox_description'  => __( 'Gives group creators and administrators the ability to attach external RSS feeds to groups.', 'cbox' ),
 			'depends'           => 'BuddyPress (>=1.2)',
-			'version'           => '1.5.1',
-			'download_url'      => 'http://github.com/cuny-academic-commons/external-group-blogs/archive/1.5.1.zip',
+			'version'           => '1.5.2',
+			'download_url'      => 'http://github.com/cuny-academic-commons/external-group-blogs/archive/1.5.2.zip',
 			'documentation_url' => 'http://commonsinabox.org/documentation/plugins/buddypress-external-group-rss',
 		) );
 
