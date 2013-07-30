865a8368641b9020bbd485cb2347306ea2a31ade
r-a-y <r-a-y@gmx.com>
2013-07-30 04:57:22 -0700

    Prep changelog in CBOX dashboard for 1.0.5.

--- admin/changelog.php
+++ admin/changelog.php
@@ -21,7 +21,14 @@
 				<div class="feature-section">
 					<h4><?php _e( 'Since the last version release, the following plugins were updated in CBOX:', 'cbox' ); ?></h4>
 					<ul>
-						<li>BuddyPress Docs 1.4.3</li>
+						<li>BuddyPress 1.8</li>
+						<li>BuddyPress Docs 1.4.5</li>
+						<li>BuddyPress Docs Wiki 1.0.4</li>
+						<li>BuddyPress Group Email Subscription 3.4</li>
+						<li>BP Groupblog 1.8.4</li>
+						<li>BuddyPress Reply By Email 1.0-RC2</li>
+						<li>Invite Anyone 1.0.23</li>
+						<li>External Group Blogs 1.5.2</li>
 					</ul>
 
 				</div>
@@ -33,7 +40,13 @@
 				<div class="feature-section">
 					<h4><?php _e( 'Since the last version release, the following bugfixes and enhancements were added to the Commons In A Box theme:', 'cbox' ); ?></h4>
 					<ul>
-						<li>Theme improvements: Fixed bug that prevented the Comments section from appearing on BuddyPress Docs pages</li>
+						<li>Fix visibility of second-level dropdown menus (Props <a href="http://commonsinabox.org/members/haystack/">Christian Wach</a>)</li>
+						<li>Various compatibility fixes for IE8 (Props <a href="http://commonsinabox.org/members/haystack/">Christian Wach</a>)</li>
+						<li>On multisite installs, allow admins to set a different front page on sub-sites</li>
+						<li>On multisite installs, allow usage of the homepage slider on sub-sites (<a href="http://commonsinabox.org/documentation/cbox-slider#multisite" target="_blank">view this article</a> for more info)</li>
+						<li>Fixed bug that prevented the Comments section from appearing on BuddyPress Docs pages</li>
+						<li>Improved compatibility with BP Docs Wiki</li>
+						<li>Fix 'New Topic' button for group forums</li>
 					</ul>
 
 				</div>
@@ -45,14 +58,20 @@
 				<div class="feature-section col two-col">
 					<div>
 
-						<p>Fixed bug in Custom Profile Filters for BuddyPress that caused some social networking fields to render incorrectly.</p>
+						<p><strong>New CBOX option:</strong> For BuddyPress group pages, you can now set the default tab to "Forum".  Try this feature by selecting it on the "Commons In A Box > Settings" page.</p>
+
+					</div>
+
+					<div class="last-feature">
+
+						<p><strong>bbPress Editor is Back!</strong> The visual editor in bbPress was disabled as of v2.3, however CBOX users can rejoice as it is automatically enabled on your site.</p>
 
 					</div>
 				</div>
 
 				<div class="feature-section col two-col">
 					<div>
-
+						<p><strong>Bug fix:</strong> Fixed bug in Custom Profile Filters for BuddyPress that caused some social networking fields to render incorrectly.</p>
 					</div>
 				</div>
 
