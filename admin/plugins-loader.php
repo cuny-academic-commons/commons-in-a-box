eb04b65b929a8a1440d5dbed8f498124ab114dce
r-a-y <r-a-y@gmx.com>
2013-07-30 04:11:56 -0700

    Pin BP Docs Wiki 1.0.4.

--- admin/plugins-loader.php
+++ admin/plugins-loader.php
@@ -156,9 +156,9 @@ class CBox_Plugins {
 			'type'              => 'recommended',
 			'cbox_name'         => __( 'Wiki', 'cbox' ),
 			'cbox_description'  => __( 'A sitewide wiki, powered by BuddyPress Docs', 'cbox' ),
-			'version'           => '1.0.3',
+			'version'           => '1.0.4',
 			'depends'           => 'BuddyPress (>=1.5), BuddyPress Docs (>=1.2)',
-			'download_url'      => 'http://github.com/boonebgorges/buddypress-docs-wiki/archive/1.0.3.zip',
+			'download_url'      => 'http://github.com/boonebgorges/buddypress-docs-wiki/archive/1.0.4.zip',
 			'documentation_url' => 'http://commonsinabox.org/documentation/plugins/buddypress-docs-wiki',
 			'network_settings'  => 'root-blog-only'
 		) );
