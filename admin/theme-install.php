8cd83f59087fcecf2c11700d7a3eb9135de0789f
Boone B Gorges <boonebgorges@gmail.com>
2015-04-28 09:33:17 -0400

    Pin cbox-theme 1.0.10.

--- admin/theme-install.php
+++ admin/theme-install.php
@@ -41,9 +41,9 @@ class CBox_Theme_Specs {
 	 */
 	private static $cbox_theme = array(
 		'name'           => 'Commons In A Box Theme',
-		'version'        => '1.0.9',
+		'version'        => '1.0.10',
 		'directory_name' => 'cbox-theme',
-		'download_url'   => 'http://github.com/cuny-academic-commons/cbox-theme/archive/1.0.9.zip'
+		'download_url'   => 'http://github.com/cuny-academic-commons/cbox-theme/archive/1.0.10.zip'
 	);
 
 	/**
