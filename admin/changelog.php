0ce5fd13b270e28006a632ad97af3fb53238e944
Boone B Gorges <boonebgorges@gmail.com>
2016-12-14 11:54:05 -0600

    Update changelog version to 1.0.14.

--- admin/changelog.php
+++ admin/changelog.php
@@ -16,8 +16,8 @@
 			</h2>
 
 			<div class="changelog">
-				<h3><?php _e( '1.0.13 Maintenance Release', 'cbox' ); ?></h3>
-				<p><?php _e( 'The 1.0.13 release brings a number of plugins up to date, and improves compatibility with WordPress 4.6 and BuddyPress 2.6.', 'cbox' ) ?></p>
+				<h3><?php _e( '1.0.14 maintenance Release', 'cbox' ); ?></h3>
+				<p><?php _e( 'The 1.0.14 release brings a number of plugins up to date, and improves compatibility with WordPress 4.7 and BuddyPress 2.7.', 'cbox' ) ?></p>
 			</div>
 
 			<div class="changelog">
@@ -26,17 +26,7 @@
 				<div class="feature-section">
 					<h4><?php _e( 'Since the last version release, the following plugins were updated in CBOX:', 'cbox' ); ?></h4>
 					<ul>
-						<li>BuddyPress 2.6.2</li>
-						<li>bbPress 2.5.10</li>
-						<li>BP Groupblog 1.8.13</li>
-						<li>BuddyPress Docs 1.9.1</li>
-						<li>BuddyPress Docs Wiki 1.9.1</li>
-						<li>BuddyPress Group Email Subscription 3.7.0</li>
-						<li>BuddyPress Reply By Email 1.0-RC4</li>
-						<li>CAC Featured Content 1.0.7</li>
-						<li>External Group Blogs 1.6.1</li>
-						<li>Invite Anyone 1.3.11</li>
-						<li>More Privacy Options 4.6</li>
+						<li></li>
 					</ul>
 
 				</div>
@@ -48,16 +38,13 @@
 				<div class="feature-section">
 					<h4><?php _e( 'Since the last version release, the following bugfixes and enhancements were added to the Commons In A Box theme:', 'cbox' ); ?></h4>
 					<ul>
-						<li><?php _e( 'Improved escaping for some strings.', 'cbox' ) ?></li>
-						<li><?php _e( 'Don\'t show settings links when Settings component is inactive.', 'cbox' ) ?></li>
-						<li><?php _e( 'Fix some PHP notices.', 'cbox' ) ?></li>
-						<li><?php _e( 'Fix bug that prevented admins from visiting a private group\'s Manage tab.', 'cbox' ) ?></li>
-						<li><?php _e( 'Fix bug with Manage Folders interface in BuddyPress Docs 1.9+', 'cbox' ) ?></li>
+						<li><?php _e( '', 'cbox' ) ?></li>
 					</ul>
 
 				</div>
 			</div>
 
+			<?php /*
 			<div class="changelog">
 				<h3><?php _e( 'Under the Hood', 'cbox' ); ?></h3>
 
@@ -70,6 +57,7 @@
 					</div>
 				</div>
 			</div>
+			*/ ?>
 
 			<div class="return-to-dashboard">
 				<?php printf( __( '<a href="%s">Return to the CBOX dashboard &rarr;</a>', 'cbox' ), self_admin_url( 'admin.php?page=cbox' ) ); ?>
