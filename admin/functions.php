c0f51a632a9289aba2d64efcf5b0d196438f3fc5
r-a-y <r-a-y@gmx.com>
2017-03-26 10:27:44 -0700

    Packages: Introduce cbox_get_template_part() and cbox_locate_template().
    Packages will be able to load custom template parts to be displayed in the
    admin area.
    
    Subsequent commits will remove hardcoded markup into templates.
    
    See #119.

--- admin/functions.php
+++ admin/functions.php
@@ -377,6 +377,94 @@ function cbox_bp_after_version_bump() {
 
 }
 
+/** TEMPLATE *************************************************************/
+
+/**
+ * Locate the highest priority CBOX admin template file that exists.
+ *
+ * Tries to see if a registered CBOX package has a template file.  If not,
+ * fall back to the 'base' template.  Similar to {@link locate_template()}.
+ *
+ * @since 1.1.0
+ *
+ * @param  string|array $template_names Template file(s) to search for, in order.
+ * @param  bool         $load           If true the template file will be loaded if it is found.
+ * @param  bool         $require_once   Whether to require_once or require. Default true. Has no effect if $load is false.
+ * @return string The template filename if one is located.
+ */
+function cbox_locate_template( $template_names, $load = false, $require_once = true ) {
+	$located = '';
+	foreach ( (array) $template_names as $template_name ) {
+		if ( ! $template_name ) {
+			continue;
+		}
+
+		$template_path = cbox_get_package_prop( 'template_path' );
+		if ( ! empty( $template_path ) && file_exists( trailingslashit( $template_path ) . $template_name ) ) {
+			$located = trailingslashit( $template_path ) . $template_name;
+			break;
+
+		} elseif ( file_exists( CBOX_PLUGIN_DIR . 'admin/templates/base/' . $template_name ) ) {
+			$located = CBOX_PLUGIN_DIR . 'admin/templates/base/' . $template_name;
+			break;
+		}
+	}
+
+	if ( $load && '' != $located ) {
+		load_template( $located, $require_once );
+	}
+
+	return $located;
+}
+
+/**
+ * Load a CBOX admin template part.
+ *
+ * Basically, almost the same as {@link get_template_part()}.
+ *
+ * @since 1.1.0
+ *
+ * @param string $slug The slug name for the generic template.
+ * @param string $name The name of the specialised template.
+ */
+function cbox_get_template_part( $slug, $name = null ) {
+	$templates = array();
+	$name = (string) $name;
+	if ( '' !== $name ) {
+		$templates[] = "{$slug}-{$name}.php";
+	}
+
+	$templates[] = "{$slug}.php";
+
+	/**
+	 * Fires before the specified template part file is loaded.
+	 *
+	 * The dynamic portion of the hook name, `$slug`, refers to the slug name
+	 * for the generic template part.
+	 *
+	 * @since 1.1.0
+	 *
+	 * @param string $slug The slug name for the generic template.
+	 * @param string $name The name of the specialized template.
+	 */
+	do_action( 'cbox_get_template_part', $slug, $name );
+
+	cbox_locate_template( $templates, true, false );
+
+	/**
+	 * Fires after the specified template part file is loaded.
+	 *
+	 * The dynamic portion of the hook name, `$slug`, refers to the slug name
+	 * for the generic template part.
+	 *
+	 * @since 1.1.0
+	 *
+	 * @param string      $slug The slug name for the generic template.
+	 * @param string|null $name The name of the specialized template.
+	 */
+	do_action( 'cbox_after_get_template_part', $slug, $name );
+}
+
 /** HOOK-RELATED ***************************************************/
 
 /**
