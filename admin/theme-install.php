0ac7e9bc5259f3d256a066f4457443c650f44e35
Boone B Gorges <boonebgorges@gmail.com>
2017-03-29 13:19:09 -0500

    Temporarily pin cbox-theme master for testing.

--- admin/theme-install.php
+++ admin/theme-install.php
@@ -49,7 +49,7 @@ class CBox_Theme_Specs {
 	 * Static bootstrapping init method.
 	 */
 	public static function init() {
-		self::$cbox_theme['download_url'] = 'http://github.com/cuny-academic-commons/cbox-theme/archive/1.0.14.zip';
+		self::$cbox_theme['download_url'] = 'http://github.com/cuny-academic-commons/cbox-theme/archive/master.zip';
 		return new self();
 	}
 
