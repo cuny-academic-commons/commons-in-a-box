280bf97b3de4bb530926079c8fec10df0c356713
r-a-y <r-a-y@gmx.com>
2017-06-17 11:28:08 -0700

    OpenLab: Add description for Braille plugin.
    See #120.

--- includes/openlab/plugins.php
+++ includes/openlab/plugins.php
@@ -273,6 +273,7 @@ class CBox_Plugins_OpenLab {
 			'plugin_name'       => 'braille',
 			'type'              => 'install-only',
 			'cbox_name'         => __( 'Braille', 'cbox' ),
+			'cbox_description'  => __( 'Provides a number of Braille-related services to WordPress and the Anthologize plugin.', 'cbox' ),
 			'version'           => '0.0.5',
 			'download_url'      => 'http://downloads.wordpress.org/plugin/braille.0.0.5.zip',
 			'documentation_url' => 'https://wordpress.org/plugins/braille',
