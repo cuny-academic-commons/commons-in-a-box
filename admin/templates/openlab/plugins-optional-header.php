e026a5de3ca06b8b7cac50740b29bd5818d36cee
r-a-y <r-a-y@gmx.com>
2017-06-17 13:41:58 -0700

    OpenLab: Override optional plugin section header.
    Mostly to change the header from 'A la carte' to 'Community Features'.
    
    See #120.

---
+++ admin/templates/openlab/plugins-optional-header.php
@@ -0,0 +1,4 @@
+	<h2><?php _e( 'Community Features', 'cbox' ); ?></h2>
+
+	<p><?php printf( __( "The following plugins influence the community aspects of your site, but they require a bit of additional setup, so we do not install them by default.", 'cbox' ), cbox_get_package_prop( 'name' ) ); ?></p>
+	<p><?php _e( "To install, check the plugins you want to install and click 'Update'.", 'cbox' ); ?></p>
