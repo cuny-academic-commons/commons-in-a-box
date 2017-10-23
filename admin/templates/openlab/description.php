7f46c1d5883a29c12eb97800d113f7b6648ef06e
r-a-y <r-a-y@gmx.com>
2017-10-23 16:07:00 -0700

    OpenLab: Alter 'description' template part.
    This gets displayed during package selection.
    
    See #120.

--- admin/templates/openlab/description.php
+++ admin/templates/openlab/description.php
@@ -1,4 +1,4 @@
 
-<p>Inspired by <a href="https://openlab.citytech.cuny.edu/" target="_blank">City Tech's OpenLab</a>, the OpenLab box is an open-source digital platform where students, faculty, and staff can meet to learn, work, and share their ideas.</p>
+<p><?php esc_html_e( 'Commons in a Box: OpenLab (CBOX-OL) is a free software project that aims to bring humanities instruction into the public square by making the work of faculty and students more connected to the outside world.', 'cbox' ); ?></p>
 
-<p>Suitable for communities catered towards teaching and learning, while enabling connections and fostering collaboration.</p>
+<p><?php esc_html_e( 'It allows members to discuss and collaborate on coursework and research, share creations publically through digital portfolios, form clubs to share initiatives and interests, and generally create a more diverse online community for your institution.', 'cbox' ); ?></p>
