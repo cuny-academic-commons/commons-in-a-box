7379de399b2d98018283a8fcef95942cb16630ab
r-a-y <r-a-y@gmx.com>
2017-10-23 16:11:08 -0700

    Packages: Modify 'footer' template part.
    Most of the footer links have been moved into the "Welcome" block at the
    top of the dashboard.
    
    The only thing left in the footer is the "Change boxes" link, which is only
    shown if the 'CBOX_SHOW_PACKAGE_SWITCH' constant is set to true.
    
    See #119.

--- admin/templates/base/footer.php
+++ admin/templates/base/footer.php
@@ -1,21 +1,6 @@
 
-		<div id="cbox-about" class="secondary-panel">
-			<h2><?php _e( 'About', 'cbox' ); ?></h2>
-
-			<p><?php printf( __( "You're currently using <strong>Commons In A Box %s</strong>", 'cbox' ), cbox_get_version() ); ?>.</p>
-
-			<p><?php printf( __( '<strong>Commons In A Box</strong> is a software project aimed at turning the infrastructure that successfully powers the <a href="%s">CUNY Academic Commons</a> into a free, distributable, easy-to-install package.', 'cbox' ), esc_url( 'http://commons.gc.cuny.edu' ) ); ?></p>
-
-			<p><?php  _e( 'Commons In A Box is made possible by a generous grant from the Alfred P. Sloan Foundation.', 'cbox' ); ?></p>
-
-			<ul>
-				<li><a href="<?php echo network_admin_url( 'admin.php?page=cbox&amp;whatsnew=1' ); ?>"><?php _e( "What's New", 'cbox' ); ?></a></li>
-				<li><a href="<?php echo network_admin_url( 'admin.php?page=cbox&amp;credits=1' ); ?>"><?php _e( 'Credits', 'cbox' ); ?></a></li>
-				<li><a href="http://commonsinabox.org/documentation/"><?php _e( 'Documentation', 'cbox' ); ?></a></li>
-				<li><a href="https://github.com/cuny-academic-commons/commons-in-a-box/commits/1.0.x"><?php _e( 'Dev tracker', 'cbox' ); ?></a></li>
-
-				<?php if ( count( cbox_get_packages() ) > 1 ) : ?>
-					<li><a class="confirm" href="<?php echo esc_url( wp_nonce_url( self_admin_url( 'admin.php?page=cbox&amp;cbox-package=0' ), 'cbox_select_package' ) ); ?>"><?php esc_html_e( 'Change boxes', 'cbox' ); ?></a></li>
-				<?php endif; ?>
-			</ul>
-		</div>
+<ul class="subsubsub">
+	<?php if ( defined( 'CBOX_SHOW_PACKAGE_SWITCH' ) && true === constant( 'CBOX_SHOW_PACKAGE_SWITCH' ) && count( cbox_get_packages() ) > 1 ) : ?>
+		<li><a class="confirm" href="<?php echo esc_url( wp_nonce_url( self_admin_url( 'admin.php?page=cbox&amp;cbox-package=0' ), 'cbox_select_package' ) ); ?>"><?php esc_html_e( 'Change boxes', 'cbox' ); ?></a></li>
+	<?php endif; ?>
+</ul>
