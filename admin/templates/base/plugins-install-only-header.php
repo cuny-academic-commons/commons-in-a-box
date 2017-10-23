d01a3400482c1ad76a87a83024f1a801f54d6253
r-a-y <r-a-y@gmx.com>
2017-10-23 11:49:16 -0700

    Plugins Admin: Add link to network-admin plugins page on "Member Site Plugins" page.

--- admin/templates/base/plugins-install-only-header.php
+++ admin/templates/base/plugins-install-only-header.php
@@ -1,3 +1,4 @@
 	
-	<p><?php _e( "The following plugins can improve the WordPress site experience for your users.", 'cbox' ); ?></p>
-	<p><?php _e( "Installing a plugin here makes it available for members in the Plugins area of their site.", 'cbox' ); ?></p>
+	<p><?php esc_html_e( "The following plugins can improve the WordPress site experience for your users.", 'cbox' ); ?></p>
+	<p><?php esc_html_e( "Installing a plugin here makes it available for members in the Plugins area of their site.", 'cbox' ); ?></p>
+	<p><?php printf( __( 'If you would like to add or manage other plugins for your members to access, you can access that area <a href="%s">here</a>.', 'cbox' ), network_admin_url( 'plugins.php' ) ); ?></p>
