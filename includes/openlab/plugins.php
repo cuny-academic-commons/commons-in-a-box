675766e2597e1003b5319b295b874e60cbe9b771
Boone B Gorges <boonebgorges@gmail.com>
2017-12-12 21:06:39 -0600

    Pin cac-featured-content 1.0.8.

--- includes/openlab/plugins.php
+++ includes/openlab/plugins.php
@@ -149,8 +149,8 @@ class CBox_Plugins_OpenLab {
 			'plugin_name'       => 'CAC Featured Content',
 			'cbox_name'         => __( 'Featured Content Widget', 'cbox' ),
 			'cbox_description'  => __( 'Provides a widget that allows you to select among five different content types to feature in a widget area.', 'cbox' ),
-			'version'           => '1.0.7',
-			'download_url'      => 'http://downloads.wordpress.org/plugin/cac-featured-content.1.0.7.zip',
+			'version'           => '1.0.8',
+			'download_url'      => 'http://downloads.wordpress.org/plugin/cac-featured-content.1.0.8.zip',
 			'documentation_url' => 'http://commonsinabox.org/documentation/plugins/cac-featured-content',
 		) );
 
