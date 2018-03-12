cuny-academic-commons/commons-in-a-box

commit 1732b5cfe0b5063a507f9d68c353959872f02ec4

Admin: Make theme activation hook less specific.
This commit moves some code specific to the Infinity theme's activation
process to the Classic package.

In the process, we also introduce a new hook,
'cbox_{PACKAGE_ID}_theme_activated', which can be used by other packages
to do something just after CBOX activates a theme on the next pageload.

See #119.

admin/admin-loader.php
@@ -70,7 +70,7 @@ class CBox_Admin {
 		// add a special header on the admin plugins page
 		add_action( 'pre_current_active_plugins', 	                            array( $this, 'plugins_page_header' ) );
 
-		// after installing the cbox-theme, run the activation hook
+		// after installing a theme, do something
 		add_action( 'admin_init',                                                   array( $this, 'theme_activation_hook' ) );
 	}
 
@@ -383,19 +383,22 @@ EOD;
 	}
 
 	/**
-         * Trigger Infinity's activation hook, if necessary
+         * Do something just after a theme is activated on the next page load.
          *
-	 * Infinity, and therefore cbox-theme, run certain setup routines at
-         * 'infinity_dashboard_activated'. However, this hook doesn't fire
-         * properly when CBOX uses switch_theme() to set the current theme.
-         * Instread, we set a flag at activation, and then check on each admin
-         * pageload to see if the theme was just activated; if so, we run the
-         * activation hook.
+         * @since 1.0-beta1
          */
 	public function theme_activation_hook() {
-		if ( function_exists( 'bp_get_option' ) && bp_get_option( '_cbox_theme_activated' ) ) {
-			bp_delete_option( '_cbox_theme_activated' );
-			do_action( 'infinity_dashboard_activated' );
+		if ( get_blog_option( cbox_get_main_site_id(), '_cbox_theme_activated' ) ) {
+			delete_blog_option( cbox_get_main_site_id(), '_cbox_theme_activated' );
+
+			/**
+			 * Do something just after a theme is activated on the next page load.
+			 *
+			 * This is a dynamic hook, based off of the current package ID.
+			 *
+			 * @since 1.1.0
+			 */
+			do_action( 'cbox_' . cbox_get_current_package_id() . '_theme_activated' );
 		}
 	}
 

includes/classic/classic.php
@@ -56,4 +56,22 @@ class CBox_Package_Classic extends CBox_Package {
 			'screenshot_url' => cbox()->plugin_url( 'admin/images/screenshot_cbox_theme.png' ),
 		);
 	}
+
+	/**
+	 * Custom hooks used during package initialization.
+	 *
+	 * @since 1.1.0
+	 */
+	protected function custom_init() {
+		/**
+	         * Trigger Infinity's activation hook
+	         *
+		 * Infinity, and therefore cbox-theme, runs certain setup routines at
+	         * 'infinity_dashboard_activated'. We need to run this hook just after CBOX
+	         * activates a theme, so we do that here.
+	         */
+		add_action( 'cbox_classic_theme_activated', function() {
+			do_action( 'infinity_dashboard_activated' );
+		} );
+	}
 }

commit c3307f24b3aaec7c8b75f8f555d01465044da23b

Fix issue with installing on a fresh single-site install.
Don't use multisite-only functions!

Broke after commits 1732b5c and 4ed77ea.

admin/admin-loader.php
@@ -213,7 +213,7 @@ class CBox_Admin {
 				 * Mark the theme as having just been activated so that we can run the setup
 				 * on next pageload
 				 */
-				update_blog_option( cbox_get_main_site_id(), '_cbox_theme_activated', '1' );
+				update_site_option( '_cbox_theme_activated', '1' );
 
 				wp_redirect( admin_url( cbox_get_theme_prop( 'admin_settings' ) ) );
 				return;
@@ -430,8 +430,8 @@ class CBox_Admin {
          * @since 1.0-beta1
          */
 	public function theme_activation_hook() {
-		if ( get_blog_option( cbox_get_main_site_id(), '_cbox_theme_activated' ) ) {
-			delete_blog_option( cbox_get_main_site_id(), '_cbox_theme_activated' );
+		if ( get_site_option( '_cbox_theme_activated' ) ) {
+			delete_site_option( '_cbox_theme_activated' );
 
 			/**
 			 * Do something just after a theme is activated on the next page load.