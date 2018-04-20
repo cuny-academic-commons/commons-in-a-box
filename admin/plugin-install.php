43e1fa24b3776a79648c2cbe94273c4eefaf1d3f
r-a-y <r-a-y@gmx.com>
2018-04-20 08:16:59 -0700

    Plugins: Backport BP Group Email Subscription activation fix to 1.0.x branch.
    See related commit ec43c9a.
    
    See #133.

--- admin/plugin-install.php
+++ admin/plugin-install.php
@@ -779,6 +779,18 @@ class CBox_Plugin_Defaults {
 
 				break;
 
+			// BP Group Email Subscription
+			case 'buddypress-group-email-subscription/bp-activity-subscription.php' :
+				if ( function_exists( 'bp_get_email_tax_type' ) && ! taxonomy_exists( bp_get_email_tax_type() ) ) {
+					register_taxonomy(
+						/** This filter is documented in /plugins/buddypress/class-buddypress.php */
+						apply_filters( 'bp_email_tax_type', 'bp-email-type' ),
+						/** This filter is documented in /plugins/buddypress/class-buddypress.php */
+						apply_filters( 'bp_email_post_type', 'bp-email' )
+					);
+				}
+
+				break;
 		}
 	}
 }
