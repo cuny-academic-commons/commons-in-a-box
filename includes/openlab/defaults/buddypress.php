8f6b225ecba0cf715a3f6c26c21f50b4da60738e
r-a-y <r-a-y@gmx.com>
2018-05-10 08:13:51 -0700

    Fix BuddyPress component defaults after activation.
    In commit a8b0a82, we introduced some functionality so packages can run
    custom code when a plugin is activated.  This code runs on the
    'activated_plugin' hook and only once.
    
    However, BuddyPress has its own updater routine that runs on 'admin_init'.
    Due to a multitude of factors that I won't explain, we need to run
    the update routine for BuddyPress before BuddyPress can run theirs on the
    next pageload.
    
    This commit does this.
    
    See #133.

--- includes/openlab/defaults/buddypress.php
+++ includes/openlab/defaults/buddypress.php
@@ -6,30 +6,10 @@
  */
 
 /**
- * Return an array of all BP components to be activated by default.
+ * Things to do after BuddyPress is activated.
  *
- * Since BuddyPress 1.7, BP has only activated the Profile and Activity
- * components on new installations. For Commons In A Box, we want to
- * keep the old behavior of turning all components on.
- *
- * @since 1.1.0 This logic was moved out of the CBOX plugins.php file.
- */
-add_filter( 'bp_new_install_default_components', function( $retval ) {
-	return array(
-		'activity'      => 1,
-		'blogs'         => 1,
-		'friends'       => 1,
-		'groups'        => 1,
-		'members'       => 1,
-		'messages'      => 1,
-		'notifications' => 1,
-		'settings'      => 1,
-		'xprofile'      => 1,
-	);
-} );
-
-/**
- * Don't let BuddyPress redirect to its about page after activating.
+ * - Don't let BuddyPress redirect to its about page after activating.
+ * - Install all BuddyPress components.
  *
  * @since 1.1.0 This logic was moved out of the CBOX plugin-install.php file.
  */
@@ -38,5 +18,24 @@ add_action( 'activated_plugin', function( $plugin ) {
 		return;
 	}
 
+	// Don't let BuddyPress redirect to its about page after activating.
 	delete_transient( '_bp_activation_redirect' );
+
+	// Run BP version updater routine with all BP components installed.
+	add_filter( 'bp_new_install_default_components', function( $retval ) {
+		return array(
+			'activity'      => 1,
+			'blogs'         => 1,
+			'friends'       => 1,
+			'groups'        => 1,
+			'members'       => 1,
+			'messages'      => 1,
+			'notifications' => 1,
+			'settings'      => 1,
+			'xprofile'      => 1,
+		);
+	} );
+	require_once buddypress()->plugin_dir . '/bp-members/bp-members-template.php';
+	require_once buddypress()->plugin_dir . '/bp-core/bp-core-update.php';
+	bp_version_updater();
 } );
