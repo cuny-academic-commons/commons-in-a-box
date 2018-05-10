ca91865ad0c8517b75fd6aad261fe6ab2d3a84c4
r-a-y <r-a-y@gmx.com>
2018-05-10 11:37:34 -0700

    Register email taxonomy during BuddyPress activation.
    In commit 8f6b225, we moved to manually running the BuddyPress updater
    routine.  However, this broke how BuddyPress creates email posts for
    HTML emails.
    
    To address this, we have to manually register BP's email taxonomy to ensure
    BuddyPress email posts are attached to email situations.  See commit
    43e1fa2 for a similar fix for the BP Group Email Subscription plugin.
    Since we are now registering the email taxonomy when BuddyPress is
    activated, we no longer need to register it during GES activation.
    
    See #133.

--- includes/openlab/defaults/buddypress.php
+++ includes/openlab/defaults/buddypress.php
@@ -21,6 +21,14 @@ add_action( 'activated_plugin', function( $plugin ) {
 	// Don't let BuddyPress redirect to its about page after activating.
 	delete_transient( '_bp_activation_redirect' );
 
+	// Register email taxonomy manually, since it's needed for BP emails.
+	register_taxonomy(
+		/** This filter is documented in /plugins/buddypress/class-buddypress.php */
+		apply_filters( 'bp_email_tax_type', 'bp-email-type' ),
+		/** This filter is documented in /plugins/buddypress/class-buddypress.php */
+		apply_filters( 'bp_email_post_type', 'bp-email' )
+	);
+
 	// Run BP version updater routine with all BP components installed.
 	add_filter( 'bp_new_install_default_components', function( $retval ) {
 		return array(
@@ -35,6 +43,7 @@ add_action( 'activated_plugin', function( $plugin ) {
 			'xprofile'      => 1,
 		);
 	} );
+
 	require_once buddypress()->plugin_dir . '/bp-members/bp-members-template.php';
 	require_once buddypress()->plugin_dir . '/bp-core/bp-core-update.php';
 	bp_version_updater();
