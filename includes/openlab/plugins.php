3856dc57bbe9229d260b304257d3eef87c6aa2fb
Boone B Gorges <boonebgorges@gmail.com>
2018-05-21 16:30:19 +0000

    Pin buddypress 3.0.0.
    Fixes cuny-academic-commons/openlab-theme#172.

--- includes/openlab/plugins.php
+++ includes/openlab/plugins.php
@@ -43,7 +43,7 @@ class CBox_Plugins_OpenLab {
 			'plugin_name'       => 'BuddyPress',
 			'cbox_name'         => __( 'BuddyPress', 'cbox' ),
 			'cbox_description'  => __( 'BuddyPress provides the core functionality of Commons In A Box, including groups and user profiles.', 'cbox' ),
-			'version'           => '2.9.4',
+			'version'           => '3.0.0',
 			'documentation_url' => 'http://commonsinabox.org/documentation/plugins/buddypress-plugin',
 			'admin_settings'    => 'options-general.php?page=bp-components',
 			'network_settings'  => 'settings.php?page=bp-components'
@@ -196,7 +196,7 @@ class CBox_Plugins_OpenLab {
 		$instance( array(
 			'plugin_name'  => 'BuddyPress',
 			'type'         => 'dependency',
-			'download_url' => 'http://downloads.wordpress.org/plugin/buddypress.2.9.4.zip'
+			'download_url' => 'http://downloads.wordpress.org/plugin/buddypress.3.0.0.zip'
 		) );
 
 		// Event Organiser
