d19893f4ed1b7362fe9571377d9bfbc1a34fe104
r-a-y <r-a-y@gmx.com>
2018-07-26 20:41:19 -0700

    OpenLab: Update Getting Started admin template part.
    See #120.

--- admin/templates/openlab/getting-started.php
+++ admin/templates/openlab/getting-started.php
@@ -9,28 +9,28 @@
 
 	<ol>
 	<li>
-		<h4><a href="<?php echo cbox_admin_prop( 'url', 'admin.php?page=cbox-ol-group-settings' ); ?>"><?php esc_html_e( 'Group Settings', 'cbox' ); ?></a></h4>
-		<p><?php esc_html_e( 'Affect the appearance and functionality of groups across your site. You can change the features and site templates available to groups, as well as create and assign categories for groups that can be used to filter items on the group directory pages.', 'cbox' ); ?></p>
+		<h4><a href="<?php echo cbox_admin_prop( 'url', 'admin.php?page=cbox-plugins' ); ?>"><?php esc_html_e( 'CBOX-OL Plugins', 'cbox' ); ?></a></h4>
+		<p><?php esc_html_e( 'Manage Community Feature plugins for your site and Member Site Plugins that you would like to make available to your members for their individual WordPress sites.', 'cbox' ); ?></p>
 	</li>
 	<li>
 		<h4><a href="<?php echo cbox_admin_prop( 'url', 'admin.php?page=cbox-ol-member-settings' ); ?>"><?php esc_html_e( 'Member Settings', 'cbox' ); ?></a></h4>
 		<p><?php esc_html_e( 'Modify member categories, their capabilities and permissions, who can create an account, and what types of accounts different members can create. These settings also include email domain whitelisting and registration codes to restrict access by account type.', 'cbox' ); ?></p>
 	</li>
 	<li>
-		<h4><a href="<?php echo cbox_admin_prop( 'url', 'admin.php?page=cbox-ol-brand-settings' ); ?>"><?php esc_html_e( 'Visual Settings', 'cbox' ); ?></a></h4>
-		<p><?php esc_html_e( 'Customize your site including color scheme, logo, homepage layout, footer content, and widgets.', 'cbox' ); ?></p>
-	</li>
-	<li>
-		<h4><a href="<?php echo cbox_admin_prop( 'url', 'admin.php?page=cbox-ol-communication-settings' ); ?>"><?php esc_html_e( 'Communication', 'cbox' ); ?></a></h4>
-		<p><?php esc_html_e( 'Manage email and other communication settings, including email template content and appearance and group email subscription settings.', 'cbox' ); ?></p>
+		<h4><a href="<?php echo cbox_admin_prop( 'url', 'admin.php?page=cbox-ol-group-settings' ); ?>"><?php esc_html_e( 'Group Settings', 'cbox' ); ?></a></h4>
+		<p><?php esc_html_e( 'Affect the appearance and functionality of groups across your site. You can change the features and site templates available to groups, as well as create and assign categories for groups that can be used to filter items on the group directory pages.', 'cbox' ); ?></p>
 	</li>
 	<li>
 		<h4><a href="<?php echo cbox_admin_prop( 'url', 'admin.php?page=cbox-ol-academic-units' ); ?>"><?php esc_html_e( 'Academic Units', 'cbox' ); ?></a></h4>
 		<p><?php esc_html_e( "Create unit-types that best describe how your institution is organized &ndash; for example, Departments that are located within Divisions &ndash; and define the units within each type. These units can be utilized as filters across your site's groups and for members to associate themselves in their profiles.", 'cbox' ); ?></p>
 	</li>
 	<li>
-		<h4><a href="<?php echo cbox_admin_prop( 'url', 'admin.php?page=cbox-plugins' ); ?>"><?php esc_html_e( 'CBOX-OL Plugins', 'cbox' ); ?></a></h4>
-		<p><?php esc_html_e( 'Manage Community Feature plugins for your site and Member Site Plugins that you would like to make available to your members for their individual WordPress sites.', 'cbox' ); ?></p>
+		<h4><a href="<?php echo cbox_admin_prop( 'url', 'admin.php?page=cbox-ol-brand-settings' ); ?>"><?php esc_html_e( 'Brand Settings', 'cbox' ); ?></a></h4>
+		<p><?php esc_html_e( 'Customize your site including color scheme, logo, homepage layout, footer content, and widgets.', 'cbox' ); ?></p>
+	</li>
+	<li>
+		<h4><a href="<?php echo cbox_admin_prop( 'url', 'admin.php?page=cbox-ol-communication-settings' ); ?>"><?php esc_html_e( 'Communication Settings', 'cbox' ); ?></a></h4>
+		<p><?php esc_html_e( 'Manage email and other communication settings, including email template content and appearance and group email subscription settings.', 'cbox' ); ?></p>
 	</li>
 	</ol>
 	</div>
