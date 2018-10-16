5e506fa87162214bd8ab9ea9afa1f9b339231bbc
r-a-y <r-a-y@gmx.com>
2018-10-16 15:53:40 -0700

    Admin: Add back "What's New" and "Credits" links to dashboard.

--- admin/templates/classic/welcome-description.php
+++ admin/templates/classic/welcome-description.php
@@ -3,5 +3,7 @@
 <p><?php printf( __( 'Commons In A Box was made possible by a generous grant from the <a href="%1$s" target="_blank">Alfred P. Sloan Foundation</a> that allowed us to turn the infrastructure that successfully powers the <a href="%2$s" target="_blank">CUNY Academic Commons</a> into a distributable, easy-to-install package.', 'cbox' ), 'https://sloan.org/', 'http://commons.gc.cuny.edu/' ); ?></p>
 
 <ul class="subsubsub">
-<li><a href="http://commonsinabox.org/documentation/"><?php _e( 'Documentation', 'cbox' ); ?></a></li>
+<li><a href="<?php echo cbox_admin_prop( 'url', 'admin.php?page=cbox&amp;whatsnew=1' ); ?>"><?php esc_html_e( "What's New", 'cbox' ); ?></a> |</li>
+<li><a href="http://commonsinabox.org/project-team" target="_blank"><?php esc_html_e( 'Credits', 'cbox' ); ?></a> |</li>
+<li><a href="http://commonsinabox.org/documentation/" target="_blank"><?php _e( 'Documentation', 'cbox' ); ?></a></li>
 </ul>
--- admin/templates/openlab/welcome-description.php
+++ admin/templates/openlab/welcome-description.php
@@ -3,5 +3,7 @@
 <p><?php printf( __( 'Commons In A Box OpenLab is a collaboration between <a href="%1$s" target="_blank">The Graduate Center, CUNY</a> and <a href="%2$s" target="_blank">New York City College of Technology</a>. It is being created with funding from the National Endowment for the Humanities\' <a href="%3$s" target="_blank">Office of Digital Humanities</a>.', 'cbox' ), 'https://www.gc.cuny.edu/', 'https://www.citytech.cuny.edu/', 'https://www.neh.gov/divisions/odh' ); ?></p>
 
 <ul class="subsubsub">
+<li><a href="<?php echo cbox_admin_prop( 'url', 'admin.php?page=cbox&amp;whatsnew=1' ); ?>"><?php esc_html_e( "What's New", 'cbox' ); ?></a> |</li>
+<li><a href="http://commonsinabox.org/project-team" target="_blank"><?php esc_html_e( 'Credits', 'cbox' ); ?></a> |</li>
 <li><a href="http://commonsinabox.org/documentation/"><?php _e( 'Documentation', 'cbox' ); ?></a></li>
 </ul>
