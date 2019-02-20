d183ddeba08f8ee43e9e206995efd67bb05cf680
Boone B Gorges <boonebgorges@gmail.com>
2019-02-20 13:35:36 -0600

    Pin buddypress 4.2.0.

--- admin/templates/classic/changelog.php
+++ admin/templates/classic/changelog.php
@@ -39,7 +39,7 @@
 						<li>BP Group Announcements 1.0.6</li>
 						<li>BP MPO Activity Filter 1.3.1</li>
 						<li>BP Groupblog 1.9.1</li>
-						<li>BuddyPress 4.1.0</li>
+						<li>BuddyPress 4.2.0</li>
 						<li>BuddyPress Docs 2.1.2</li>
 						<li>Invite Anyone 1.4.0</li>
 					</ul>
