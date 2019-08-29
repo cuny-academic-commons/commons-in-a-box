58f157b5091f34464d5e686a14414f251de0e621
Boone B Gorges <boonebgorges@gmail.com>
2019-08-29 14:44:20 +0000

    1.1.2 version bumps.

--- loader.php
+++ loader.php
@@ -3,7 +3,7 @@
 Plugin Name: Commons In A Box
 Plugin URI: http://commonsinabox.org
 Description: A suite of community and collaboration tools for WordPress, designed especially for academic communities
-Version: 1.1.1
+Version: 1.1.2
 Author: CUNY Academic Commons
 Author URI: http://commons.gc.cuny.edu
 Licence: GPLv3
@@ -84,10 +84,10 @@ class Commons_In_A_Box {
 		/** VERSION ***********************************************************/
 
 		// CBOX version
-		$this->version       = '1.1.1';
+		$this->version       = '1.1.2';
 
 		// UTC date of CBOX version release
-		$this->revision_date = '2019-03-05 15:00 UTC';
+		$this->revision_date = '2019-08-29 15:00 UTC';
 
 		/** FILESYSTEM ********************************************************/
 
--- readme.txt
+++ readme.txt
@@ -2,8 +2,8 @@
 Contributors: cuny-academic-commons, boonebgorges, r-a-y
 Tags: commons, cuny, community, education, academic, buddypress
 Requires at least: 4.9.8
-Tested up to: 5.1
-Stable tag: 1.1.1
+Tested up to: 5.2
+Stable tag: 1.1.2
 License: GPLv3
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 
