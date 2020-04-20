8f9abeebe4e044e771cb1f7ff3d32ac5084ed14a
r-a-y <r-a-y@gmx.com>
2020-04-20 15:13:11 -0700

    Frontend: Remove duplicate activity entry workaround for bbPress 2.5.
    No longer needed for bbPress 2.6.
    
    See #199.

--- includes/frontend-bbpress.php
+++ includes/frontend-bbpress.php
@@ -42,8 +42,6 @@ class CBox_BBP_Autoload {
 
 		$this->enable_visual_editor();
 
-		$this->get_activity_id_hotfix();
-
 		$this->fix_form_actions();
 
 		$this->save_notification_meta();
@@ -143,85 +141,6 @@ class CBox_BBP_Autoload {
 
 		// wipe out the second row of TinyMCE buttons
 		add_filter( 'mce_buttons_2', '__return_empty_array' );
-	}
-
-	/**
-	 * Hotfix for bbPress activity ID fetching.
-	 *
-	 * Changes to BuddyPress in v2.1 to remove the activity count from activity
-	 * queries broke how bbPress determines what the existing activity ID is.
-	 *
-	 * This is a hotfix to add the new 'count_total' parameter added in BP 2.1, so
-	 * bbPress can accurately fetch the correct activity ID.  This prevents
-	 * duplicate activity entries when editing a bbPress forum post.
-	 *
-	 * This should hopefully be addressed in bbPress 2.6. Remove this in CBOX 1.1.
-	 *
-	 * @since 1.0.9
-	 *
-	 * @see BBP_BuddyPress_Activity::get_activity_id()
-	 * @see https://bbpress.trac.wordpress.org/ticket/2690
-	 */
-	public function get_activity_id_hotfix() {
-		// stop if BP is lower than 2.1
-		if ( version_compare( BP_VERSION, '2.1.0' ) < 0 ) {
-		        return;
-		}
-
-		// we're hoping a fix is going to be ready by bbP 2.6
-		if ( version_compare( bbp_get_version(), '2.6' ) >= 0 ) {
-		        return;
-		}
-
-		add_filter( 'get_post_metadata', array( $this, 'add_activity_get_specific_hotfix' ), 10, 3 );
-		add_filter( 'bp_activity_total_activities_sql', array( $this, 'remove_activity_get_specific_hotfix' ) );
-	}
-
-	/**
-	 * Add filter to bp_activity_get_specific() to add 'count_total' parameter.
-	 *
-	 * Before bbPress determines what the activity ID is, it tries to fetch the
-	 * activity ID from its post meta.  This is a good place to add in our
-	 * bp_activity_get_specific() filter before the function is called.
-	 *
-	 * Remove this in CBOX 1.1.
-	 *
-	 * @see BBP_BuddyPress_Activity::get_activity_id()
-	 */
-	public function add_activity_get_specific_hotfix( $retval, $object_id, $meta_key ) {
-		if ( '_bbp_activity_id' === $meta_key ) {
-			add_filter( 'bp_activity_get_specific', array( $this, 'add_count_total_to_get_specific' ), 10, 3 );
-		}
-
-		return $retval;
-	}
-
-	/**
-	 * Add back the 'count_total' parameter when using bp_activity_get_specific().
-	 *
-	 * Because activity counts were removed by default in BuddyPress 2.1, this
-	 * broke how bbPress determines what an existing activity ID is.  This method
-	 * adds back activity counts when using bp_activity_get_specific() so bbPress
-	 * will function like before.
-	 *
-	 * Remove this in CBOX 1.1.
-	 */
-	public function add_count_total_to_get_specific( $retval, $orig_args, $r ) {
-		$r['count_total'] = true;
-		return BP_Activity_Activity::get( $r );
-	}
-
-	/**
-	 * Remove filter to bp_activity_get_specific() to add 'count_total' parameter.
-	 *
-	 * Remove this in CBOX 1.1.
-	 *
-	 * @see CBox_BBP_Autoload::add_activity_get_specific_hotfix()
-	 */
-	public function remove_activity_get_specific_hotfix( $retval ) {
-		remove_filter( 'bp_activity_get_specific', array( $this, 'add_count_total_to_get_specific' ), 10, 3 );
-
-		return $retval;
 	}
 
 	/**
