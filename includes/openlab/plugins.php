1763431385625ab1574760aa388c1ff8bc68c7b6
Boone B Gorges <boonebgorges@gmail.com>
2020-07-24 15:11:30 +0000

    Add openlab-badges to openlab plugin manifest.
    Using tag 1.0.0-beta1 for testing.
    
    See #203.

--- includes/openlab/plugins.php
+++ includes/openlab/plugins.php
@@ -184,6 +184,15 @@ class CBox_Plugins_OpenLab {
 			'version'           => '1.0.0',
 			'download_url'      => 'https://github.com/livinglab/openlab-portfolio/archive/1.0.0.zip',
 		) );
+
+		// OpenLab Badges
+		call_user_func( $instance, array(
+			'plugin_name'       => 'OpenLab Badges',
+			'cbox_name'         => __( 'Badges', 'commons-in-a-box' ),
+			'cbox_description'  => __( 'Customizable badges for your OpenLab groups.', 'commons-in-a-box' ),
+			'version'           => '1.0.0-beta1',
+			'download_url'      => 'https://github.com/livinglab/openlab-badges/archive/1.0.0-beta1.zip',
+		) );
 	}
 
 	/**
