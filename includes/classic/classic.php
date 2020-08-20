19f2e2c25c3c4fe480c2f3291b8d1bb41f7ce286
Boone B Gorges <boonebgorges@gmail.com>
2020-08-20 14:54:23 +0000

    Pin cbox-theme 1.2.0.

--- includes/classic/classic.php
+++ includes/classic/classic.php
@@ -49,9 +49,9 @@ class CBox_Package_Classic extends CBox_Package {
 	protected static function theme() {
 		return array(
 			'name'           => 'Commons In A Box',
-			'version'        => '1.2.0-beta1',
+			'version'        => '1.2.0',
 			'directory_name' => 'cbox-theme',
-			'download_url'   => 'http://github.com/cuny-academic-commons/cbox-theme/archive/1.2.0-beta1.zip',
+			'download_url'   => 'http://github.com/cuny-academic-commons/cbox-theme/archive/1.2.0.zip',
 			'admin_settings' => 'themes.php?page=infinity-theme',
 			'screenshot_url' => cbox()->plugin_url( 'admin/images/screenshot_cbox_theme.png' ),
 		);
