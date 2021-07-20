624aae53b17f18b18dc6d1a3c7218cc7e6b0ad41
Boone B Gorges <boonebgorges@gmail.com>
2021-07-20 09:30:57 -0500

    Classic: Pin external-group-blogs 1.6.2.

--- includes/classic/plugins.php
+++ includes/classic/plugins.php
@@ -240,8 +240,8 @@ class CBox_Plugins_Classic {
 			'cbox_name'         => __( 'External RSS Feeds for Groups', 'commons-in-a-box' ),
 			'cbox_description'  => __( 'Gives group creators and administrators the ability to attach external RSS feeds to groups.', 'commons-in-a-box' ),
 			'depends'           => 'BuddyPress (>=1.5)',
-			'version'           => '1.6.1',
-			'download_url'      => 'http://github.com/cuny-academic-commons/external-group-blogs/archive/1.6.1.zip',
+			'version'           => '1.6.2',
+			'download_url'      => 'http://github.com/cuny-academic-commons/external-group-blogs/archive/1.6.2.zip',
 			'documentation_url' => 'http://commonsinabox.org/documentation/plugins/buddypress-external-group-rss',
 			'network'           => false
 		) );
