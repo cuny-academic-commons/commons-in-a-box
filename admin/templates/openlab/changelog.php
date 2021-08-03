c9d90de417d8516b414750c2eb6c4b5576d24dcd
Boone B Gorges <boonebgorges@gmail.com>
2021-08-03 10:31:04 -0500

    OpenLab: 1.3.0 admin changelog.

--- admin/templates/openlab/changelog.php
+++ admin/templates/openlab/changelog.php
@@ -22,11 +22,17 @@
 
 				<div class="feature-section col two-col">
 					<ul>
-						<li><?php esc_html_e( 'Compatibility with WordPress 5.6+.', 'commons-in-a-box' ); ?></li>
-						<li><?php esc_html_e( 'Fixed the logic of the default "Term" value when creating a Course.', 'commons-in-a-box' ); ?></li>
-						<li><?php esc_html_e( 'Fixed bug that made it impossible to save an empty value for "Term" in a Course.', 'commons-in-a-box' ); ?></li>
-						<li><?php esc_html_e( 'Improved behavior when populating the "Clone Existing" dropdown during group creation.', 'commons-in-a-box' ); ?></li>
-						<li><?php esc_html_e( 'Better flushing of permalinks on newly created sites.', 'commons-in-a-box' ); ?></li>
+						<li><?php esc_html_e( 'Compatibility with WordPress 5.8+ and BuddyPress 9.0+.', 'commons-in-a-box' ); ?></li>
+						<li><?php esc_html_e( 'Introduce the "Creators" feature, allowing greater customization over the way a group\'s Acknowledgements are displayed and inherited by clones.', 'commons-in-a-box' ); ?></li>
+						<li><?php esc_html_e( 'Introduce the "OpenLab Private Comments" site plugin, which allows users to post comments that are visible only to the site owner.', 'commons-in-a-box' ); ?></li>
+						<li><?php esc_html_e( 'Introduce the "OpenLab Attributions" site plugins, a powerful tool for generating and displaying inline attributions into post content.', 'commons-in-a-box' ); ?></li>
+						<li><?php esc_html_e( 'Introduce the Creative Commons widget.', 'commons-in-a-box' ); ?></li>
+						<li><?php esc_html_e( 'Introduce a toggle that allows site admins to disable the OpenLab toolbar for logged-out users visiting their sites.', 'commons-in-a-box' ); ?></li>
+						<li><?php esc_html_e( 'Fixed bug that could cause duplicate bbPress forums to be created for a group.', 'commons-in-a-box' ); ?></li>
+						<li><?php esc_html_e( 'Fixed bug that prevented Pending forum topics from being viewed on the front end.', 'commons-in-a-box' ); ?></li>
+						<li><?php esc_html_e( 'Add an admin email notification when a forum post is marked as Pending.', 'commons-in-a-box' ); ?></li>
+						<li><?php esc_html_e( 'Improved behavior when restoring a forum topic from trash/spam.', 'commons-in-a-box' ); ?></li>
+						<li><?php esc_html_e( 'Improve bbPress redirect behavior after CBOX installation.', 'commons-in-a-box' ); ?></li>
 					</ul>
 				</div>
 			</div>
@@ -36,7 +42,12 @@
 
 				<div class="feature-section">
 					<ul>
-						<li>BuddyPress 6.4.0</li>
+						<li>BP Reply By Email 1.0-RC10</li>
+						<li>BuddyPress 9.0.0</li>
+						<li>BuddyPress Docs 2.1.6</li>
+						<li>Event Organiser 3.10.7</li>
+						<li>PressForward 5.2.6</li>
+						<li>WP Grade Comments 1.4.0</li>
 					</ul>
 				</div>
 			</div>
@@ -46,10 +57,11 @@
 
 				<div class="feature-section">
 					<ul>
-						<li><?php esc_html_e( 'Compatibility with latest versions of jQuery included in WordPress 5.6+.', 'commons-in-a-box' ); ?></li>
-						<li><?php esc_html_e( 'Improvements to the behavior of nav menus on medium-width screens.', 'commons-in-a-box' ); ?></li>
-						<li><?php esc_html_e( 'Fixed "Sent between" text that appears on private message threads.', 'commons-in-a-box' ); ?></li>
-						<li><?php esc_html_e( 'Better consistency when generating group navigation for group Moderators.', 'commons-in-a-box' ); ?></li>
+						<li><?php esc_html_e( 'Introduce a clone "counter", which shows a tally of a group\'s descendants.', 'commons-in-a-box' ); ?></li>
+						<li><?php esc_html_e( 'Improvements for group search, including respect for quoted phrases.', 'commons-in-a-box' ); ?></li>
+						<li><?php esc_html_e( 'Introduce Delete buttons to Message panels.', 'commons-in-a-box' ); ?></li>
+						<li><?php esc_html_e( 'Improvements to the way fields are pre-filled and validated when cloning an existing group.', 'commons-in-a-box' ); ?></li>
+						<li><?php esc_html_e( 'Accessibility and contrast improvements to the Messages panel.', 'commons-in-a-box' ); ?></li>
 					</ul>
 				</div>
 			</div>
