0fd79c714db6dba46da010d62455e3f40ecb5ecd
Boone B Gorges <boonebgorges@gmail.com>
2022-12-19 09:51:47 -0600

    Fix typo in URL on OpenLab welcome screen.
    See #418.

--- admin/templates/openlab/welcome-description.php
+++ admin/templates/openlab/welcome-description.php
@@ -4,7 +4,7 @@
 printf(
 	// translators: 1. URL of the CUNY Graduate Center; 2. URL of New York City College of Technology; 3. URL of NEH ODH, 4. URL of the CUNY OER initiative, 5. URL of the BMCC OpenLab
 	__( 'It was created through a collaboration between <a href="%1$s" target="_blank">The Graduate Center, CUNY</a> and <a href="%2$s" target="_blank">New York City College of Technology, CUNY</a> with funding from the National Endowment for the Humanities’ <a href="%3$s" target="_blank">Office of Digital Humanities</a>. Funding for continued development of new features has been provided by the <a href="%4$s" target="_blank">CUNY Open Educational Resources Initiative</a> and our partners at the <a href="%5$s" target="_blank">BMCC OpenLab</a>.', 'commons-in-a-box' ),
-	'http.://www.gc.cuny.edu/',
+	'https://www.gc.cuny.edu/',
 	'https://www.citytech.cuny.edu/',
 	'https://www.neh.gov/divisions/odh',
 	'https://www.cuny.edu/libraries/open-educational-resources/',
