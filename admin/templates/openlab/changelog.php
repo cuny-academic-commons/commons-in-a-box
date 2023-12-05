7ad2ba0cd79a358a1ce939ce3a4eab07b1616707
Boone B Gorges <boonebgorges@gmail.com>
2023-12-05 10:09:41 -0600

    OpenLab: Pin buddypress-docs 2.2.0.
    See #458.

--- admin/templates/openlab/changelog.php
+++ admin/templates/openlab/changelog.php
@@ -37,15 +37,7 @@
 
 				<div class="feature-section">
 					<ul>
-						<li>BuddyPress 11.2.0</li>
-						<li>BuddyPress Group Email Subscription 4.1.0</li>
-						<li>BP Group Documents 1.22</li>
-						<li>Event Organiser 3.12.3</li>
-						<li>Invite Anyone 1.4.4</li>
-						<li>OpenLab Attributions 2.1.2</li>
-						<li>OpenLab Portfolio 1.1.2</li>
-						<li>PressForward 5.5.0</li>
-						<li>WP Grade Comments 1.5.0</li>
+						<li>BuddyPress Docs 2.2.0</li>
 					</ul>
 				</div>
 			</div>
