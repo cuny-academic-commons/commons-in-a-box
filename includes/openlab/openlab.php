098e7306fcba24c9f138b142b46b54620e4f5388
Boone B Gorges <boonebgorges@gmail.com>
2024-08-05 12:45:50 -0500

    OpenLab: Pin openlab-theme 1.6.0-beta1.

--- includes/openlab/openlab.php
+++ includes/openlab/openlab.php
@@ -63,9 +63,9 @@ class CBox_Package_OpenLab extends CBox_Package {
 	protected static function theme() {
 		return array(
 			'name'           => 'CBOX OpenLab',
-			'version'        => '1.5.0',
+			'version'        => '1.6.0-beta1',
 			'directory_name' => 'openlab-theme',
-			'download_url'   => 'http://github.com/cuny-academic-commons/openlab-theme/archive/1.5.0.zip',
+			'download_url'   => 'http://github.com/cuny-academic-commons/openlab-theme/archive/1.6.0-beta1.zip',
 			'screenshot_url' => cbox()->plugin_url( 'admin/images/screenshot_openlab_theme.png' ),
 			'force_install'  => true
 		);
