80270b016fb2780857378a0558764f424ffe1e27
Boone B Gorges <boonebgorges@gmail.com>
2026-01-08 10:57:19 -0600

    1.7.1 version bumps.

--- loader.php
+++ loader.php
@@ -3,7 +3,7 @@
 Plugin Name: Commons In A Box
 Plugin URI: http://commonsinabox.org
 Description: A suite of community and collaboration tools for WordPress, designed especially for academic communities
-Version: 1.7.1-beta1
+Version: 1.7.1
 Author: CUNY Academic Commons
 Author URI: http://commons.gc.cuny.edu
 Licence: GPLv3
@@ -163,10 +163,10 @@ class Commons_In_A_Box {
 		/** VERSION ***********************************************************/
 
 		// CBOX version
-		$this->version       = '1.7.1-beta1';
+		$this->version       = '1.7.1';
 
 		// UTC date of CBOX version release
-		$this->revision_date = '2026-01-05 14:00 UTC';
+		$this->revision_date = '2026-01-08 15:00 UTC';
 
 		/** FILESYSTEM ********************************************************/
 
